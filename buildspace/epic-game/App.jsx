ed0b97e407f3b3ab2d9a15446d468502787284f9
David Lee <davidtaolee@gmail.com>
2022-01-31 17:27:38 +0800

    Move getContract to a function
    a little tweak on the error handling

--- buildspace/epic-game/App.jsx
+++ buildspace/epic-game/App.jsx
@@ -17,6 +17,28 @@ const App = () => {
   //Right under current account, setup this new state property
   const [characterNFT, setCharacterNFT] = useState(null);
 
+  function getContract(ethereum){
+    if (!ethereum) {
+        console.log('Make sure you have MetaMask yo~');
+        return null;
+    }else{
+      console.log("ethereum is good");
+      if (ethereum.networkVersion !== '4') {
+        alert("Please connect to Rinkeby!");
+        return null;
+      } 
+    }
+
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        myEpicGame.abi,
+        signer
+      );
+    return contract;
+  }
+
   // Start by creating a new action that we will run on component load
   // Actions
   const checkIfWalletIsConnected = async () => {
@@ -28,10 +50,7 @@ const App = () => {
         return;
       } else {
         console.log('We have the ethereum object', ethereum);
-        if (ethereum.networkVersion !== '4') {
-          alert("Please connect to Rinkeby!");
-          return;
-        } 
+
         const accounts = await ethereum.request({ method: 'eth_accounts' });
 
         if (accounts.length !== 0) {
@@ -113,28 +132,19 @@ const App = () => {
     //The function we will call that interacts with out smart contract
     const fetchNFTMetadata = async () => {
       console.log('Checking for Character NFT on address:', currentAccount);
-      if (!window.ethereum) {
-        console.log('Make sure you have MetaMask yo~');
+
+      const gameContract = getContract(window.ethereum)
+      if (!gameContract){
         return;
       }else{
-        console.log("ethereum is good")
+        const txn = await gameContract.checkIfUserHasNFT();
+        if (txn.name) {
+          console.log('User has character NFT');
+          setCharacterNFT(transformCharacterData(txn));
+        } else {
+          console.log('No character NFT found');
+        }
       }
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const gameContract = new ethers.Contract(
-        CONTRACT_ADDRESS,
-        myEpicGame.abi,
-        signer
-      );
-      
-      const txn = await gameContract.checkIfUserHasNFT();
-      if (txn.name) {
-        console.log('User has character NFT');
-        setCharacterNFT(transformCharacterData(txn));
-      } else {
-        console.log('No character NFT found');
-      }
-      
     };
 
     //We only want to run this, if we have a connected wallet
